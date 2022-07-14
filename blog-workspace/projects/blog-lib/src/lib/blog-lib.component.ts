882c15eba9f906cbcc492c002e43059c6d06cba6
omarwa <emaildoomar@gmail.com>
2022-07-14 14:52:20 -0300

    Adicionado Blog Lib

---
+++ blog-workspace/projects/blog-lib/src/lib/blog-lib.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'way-blog-lib',
+  template: `
+    <p>
+      blog-lib works!
+    </p>
+  `,
+  styles: [
+  ]
+})
+export class BlogLibComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
