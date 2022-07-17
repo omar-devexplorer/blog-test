c3d78b512ceebe5f8c543e04f184e755882d8cb0
omarwa <emaildoomar@gmail.com>
2022-07-17 19:16:43 -0300

    Adicionado tratamento de erros

--- blog-workspace/projects/blog-app/src/app/core/services/blog.service.ts
+++ blog-workspace/projects/blog-app/src/app/core/services/blog.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable, Subject, take, tap } from 'rxjs';
+import { Observable, Subject, catchError, throwError } from 'rxjs';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 import { Post, PostComment, User } from 'blog-lib';
 
@@ -13,22 +14,30 @@ export class BlogService {
 
   private readonly url = 'http://localhost:3000';
 
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient, private snackbar: MatSnackBar) {}
 
   getPost(): Observable<Post> {
-    return this.http.get<Post>(`${this.url}/post`);
+    return this.http
+      .get<Post>(`${this.url}/post`)
+      .pipe(catchError((err) => this.handleError(err)));
   }
 
   getUser(userId: number): Observable<User> {
-    return this.http.get<User>(`${this.url}/users/${userId}`);
+    return this.http
+      .get<User>(`${this.url}/users/${userId}`)
+      .pipe(catchError((err) => this.handleError(err)));
   }
 
   getUsers(): Observable<User[]> {
-    return this.http.get<User[]>(`${this.url}/users`);
+    return this.http
+      .get<User[]>(`${this.url}/users`)
+      .pipe(catchError((err) => this.handleError(err)));
   }
 
   createComment(post: Post): Observable<Post> {
-    return this.http.post<Post>(`${this.url}/post`, post);
+    return this.http
+      .post<Post>(`${this.url}/post`, post)
+      .pipe(catchError((err) => this.handleError(err)));
   }
 
   createTree(comments: PostComment[], parent: number | null): PostComment[] {
@@ -51,5 +60,15 @@ export class BlogService {
 
   changePost(post: Post): void {
     this.post.next({ ...post });
+  }
+
+  private handleError(err: any) {
+    this.snackbar.open(
+      'Verifique se o server está ativo. Para ativar execute: "json-server --watch db.json" na pasta db. ' +
+        err.message,
+      'Ok'
+    );
+
+    return throwError(() => err);
   }
 }
